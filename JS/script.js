6a9a44c181ecb4294a05843111c927df54f88d24
FabiDiasL <fabi_lima_@hotmail.com>
2023-06-06 10:43:34 -0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.querySelector('.card');
+    const homePage = document.getElementById('home');
+    const aboutPage = document.getElementById('about');
+    const aboutBtn = document.getElementById('aboutBtn');
+    const homeBtn = document.getElementById('homeBtn');
+
+loginForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const usuario = document.getElementById('usuario').value;
+    const senha = document.getElementById('password').value;
+
+    if (usuario === 'Fabi' && senha === '909090') {
+        loginForm.reset();                       
+        document.location.href='home.html';
+    } else {
+        alert('Usuário ou senha inválidos!');
+        }
+    });
+
+});
